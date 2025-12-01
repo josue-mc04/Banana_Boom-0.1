299e1ffb19c1720e9f2b8dab1febf5d4b5e98708
YitssTG <ismaelhuaman300@gmail.com>
2025-11-30 21:35:12 -0500

    Local Multiplayer Implemented

--- Folder_ProyectoUnity/Assets/Scrips/Player/PlayerControler.cs
+++ Folder_ProyectoUnity/Assets/Scrips/Player/PlayerControler.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Events;
 
 public class PlayerControler : MonoBehaviour, IThrowAble
 {
@@ -17,12 +17,11 @@ public class PlayerControler : MonoBehaviour, IThrowAble
     [Header("Movement Player")]
     [SerializeField] private float speed = 5f;
     [SerializeField] private float runSpeed = 8f;
-
     protected bool isRun;
     protected Vector2 moveInput;
 
     [Header("Jump Player")]
-    [SerializeField] private float jumpForce = 7f; 
+    [SerializeField] private float jumpForce = 7f;
     [SerializeField] private int maxJump = 2;
     private int jumpCount;
     private Rigidbody rb;
@@ -30,7 +29,6 @@ public class PlayerControler : MonoBehaviour, IThrowAble
 
     [Header("Climb Player")]
     [SerializeField] private float climbSpeed;
-
     private bool isClimb;
 
     [Header("Raycast")]
@@ -39,12 +37,23 @@ public class PlayerControler : MonoBehaviour, IThrowAble
 
     [Header("Physics")]
     [SerializeField] private float normalMass = 1f;
-    [SerializeField] private float airMass = 1.5f; 
+    [SerializeField] private float airMass = 1.5f;
 
     public bool isKnockback;
     public float knockbackDuration;
-
     private bool isGrounded;
+
+    [Header("Look / Camera")]
+    [SerializeField] private float lookSensitivity = 3f;
+    private Vector2 lookInput;
+    private float rotX;
+
+    [Header("Unity Events")]
+    public UnityEvent<Vector2> OnMoveEvent;
+    public UnityEvent OnJumpEvent;
+    public UnityEvent<Vector2> OnLookEvent;
+    public UnityEvent<bool> OnClimbEvent;
+    public UnityEvent OnDieEvent;
 
     private void Awake()
     {
@@ -57,9 +66,7 @@ public class PlayerControler : MonoBehaviour, IThrowAble
         {
             Camera cam = Camera.main;
             if (cam != null)
-            {
                 cameraTransform = cam.transform;
-            }
         }
 
         Cursor.lockState = CursorLockMode.Locked;
@@ -67,9 +74,7 @@ public class PlayerControler : MonoBehaviour, IThrowAble
 
         _currentHealth = _maxHealth;
         if (_healthbar != null)
-        {
             _healthbar.UpdateHealthbar(_maxHealth, _currentHealth);
-        }
 
         rb.mass = normalMass;
     }
@@ -82,42 +87,27 @@ public class PlayerControler : MonoBehaviour, IThrowAble
             Vector3 direccion = new Vector3(moveInput.x, 0, moveInput.y).normalized;
             float velocidadActual = isRun ? runSpeed : speed;
 
-            Vector3 localDir = transform.TransformDirection(direccion);
-            localDir *= velocidadActual;
-
+            Vector3 localDir = transform.TransformDirection(direccion) * velocidadActual;
             Vector3 currentVelocity = rb.linearVelocity;
             localDir.y = currentVelocity.y;
-
             rb.linearVelocity = localDir;
         }
         else if (!isKnockback)
         {
             Vector3 direccion = new Vector3(moveInput.x, 0, moveInput.y).normalized;
-            Vector3 localDir = transform.TransformDirection(direccion);
-            localDir *= speed * 0.5f; 
-
+            Vector3 localDir = transform.TransformDirection(direccion) * (speed * 0.5f);
             rb.AddForce(localDir * 10f, ForceMode.Force);
         }
         #endregion
 
         #region Ground Check
         isGrounded = Physics.Raycast(transform.position, Vector3.down, distance, layer);
+        Debug.DrawRay(transform.position, Vector3.down * distance, isGrounded ? Color.red : Color.green);
+
+        rb.mass = isGrounded ? normalMass : airMass;
 
         if (isGrounded)
-        {
-            Debug.DrawRay(transform.position, Vector3.down * distance, Color.red);
-            rb.mass = normalMass;
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, Vector3.down * distance, Color.green);
-            rb.mass = airMass;
-        }
-
-        if (isGrounded)
-        {
             jumpCount = 0;
-        }
         #endregion
 
         #region Jump
@@ -127,7 +117,6 @@ public class PlayerControler : MonoBehaviour, IThrowAble
             {
                 rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
                 rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
-
                 jumpCount++;
                 canJump = false;
             }
@@ -135,40 +124,41 @@ public class PlayerControler : MonoBehaviour, IThrowAble
         #endregion
 
         if (rb.linearVelocity.y < 0)
-        {
-            rb.linearVelocity += new Vector3(0,-25.0f,0) * Time.deltaTime;
-        }
-
-        transform.forward = GetLookDir();
+            rb.linearVelocity += new Vector3(0, -25.0f, 0) * Time.deltaTime;
     }
 
-    public void Throw()
+    private void LateUpdate()
     {
-        isKnockback = true;
-        Invoke("DisableKnockback", knockbackDuration);
+        // Rotación cámara (pitch)
+        rotX -= lookInput.y * lookSensitivity;
+        rotX = Mathf.Clamp(rotX, -80f, 80f);
+        if (cameraTransform != null)
+            cameraTransform.localRotation = Quaternion.Euler(rotX, 0, 0);
+
+        // Rotación jugador (yaw)
+        transform.Rotate(Vector3.up * lookInput.x * lookSensitivity);
     }
 
-    public void DisableKnockback()
-    {
-        isKnockback = false;
-    }
-
-    public Vector3 GetLookDir()
-    {
-        Vector3 LookDir = (transform.position - cameraTransform.position).normalized;
-        LookDir.y = 0;
-        return LookDir;
-    }
-
+    // ---------------------- INPUTS ----------------------
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
+        OnMoveEvent?.Invoke(moveInput);
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
         if (context.performed)
+        {
             canJump = true;
+            OnJumpEvent?.Invoke();
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+        OnLookEvent?.Invoke(lookInput);
     }
 
     public void OnRun(InputAction.CallbackContext context)
@@ -176,15 +166,18 @@ public class PlayerControler : MonoBehaviour, IThrowAble
         isRun = context.performed;
     }
 
+    // ---------------------- CLIMB ----------------------
     public void Climb(bool value)
     {
         isClimb = value;
         rb.useGravity = !value;
-
         if (value)
             rb.linearVelocity = Vector3.zero;
+
+        OnClimbEvent?.Invoke(value);
     }
 
+    // ---------------------- VIDA ----------------------
     public void TakeDamage(float damage)
     {
         if (damage <= 0) return;
@@ -207,6 +200,19 @@ public class PlayerControler : MonoBehaviour, IThrowAble
         if (_deathEffect != null)
             Instantiate(_deathEffect, transform.position, Quaternion.identity);
 
+        OnDieEvent?.Invoke();
         Destroy(gameObject);
+    }
+
+    // ---------------------- KNOCKBACK ----------------------
+    public void Throw()
+    {
+        isKnockback = true;
+        Invoke("DisableKnockback", knockbackDuration);
+    }
+
+    public void DisableKnockback()
+    {
+        isKnockback = false;
     }
 }
